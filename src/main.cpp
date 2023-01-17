f33e84a3a32cf2b29aac36b3bc94c6ddcae86be0
Daryl Stark <daryl@dstark.nl>
2023-01-17 16:46:17 +0100

    Added the possibility to use `dynamic_cast` in `platformio.ini` and used it to get the state of the portal

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,24 @@
 #include <Arduino.h>
 
-#include <dsl_esp_wifimanager.h>
+#include <dsl_esp_wifimanager_portal.h>
 
 using dsl::esp::apps::AppBase;
-using dsl::esp::apps::WiFiManager;
+using dsl::esp::apps::WiFiManagerPortal;
 
 AppBase *app;
 
 void setup()
 {
-    app = new WiFiManager("Smart Water Meter", 115200);
+    app = new WiFiManagerPortal("Smart Water Meter", 115200);
     app->setup();
+    WiFiManagerPortal *wifiportal = dynamic_cast<WiFiManagerPortal *>(app);
+    if (wifiportal)
+    {
+        if (wifiportal->get_mode() == WiFiManagerPortal::ManagerMode::Connected)
+        {
+            Serial.println("WiFi portal is connected!");
+        }
+    }
 }
 
 void loop()
